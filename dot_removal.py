f40b39d8701c1af16b792373abf635892f6cf807
mikemarom <mike.marom@gmail.com>
2023-09-03 00:43:52 -0700

    Elegant sub-string seach

--- dot_removal.py
+++ dot_removal.py
@@ -2,16 +2,14 @@
 import os
 
 # get the list of all files and directories
-path = "/Users/mmarom/Downloads/"
+path = "/Users/mmarom/test_files/"
 dir_list = os.listdir(path)
-
-print ("Files in '",path,"' are:")
 
 #print only the files, not directories
 for each_file in dir_list:
-	if os.path.isfile(path+each_file):  # only if file
-		print (each_file)
-		print (each_file.rfind("."))
+    if os.path.isfile(path+each_file):  # only if file
+        new_filename = each_file.replace(".", " ", each_file.count('.')-1) # remove all the dots, other than the last one, from the filename
+        os.rename(path+each_file,path+new_filename) # change old filename to new one
 		
 		
 		
